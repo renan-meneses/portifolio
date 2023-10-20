5c2c6126f10648b6efe3569bca1aff2d477ea62e
Renan Meneses <renanmenesesdev@gmail.com>
2023-10-20 09:31:12 -0300

    :bug: fix: s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,7 @@ export default function Home() {
         <Info/>
         <div className="buttons">
         <div className="educational-info ">
-        <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
-        <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
-        <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
-        <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
-        <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
-          </div>
+        </div>
           <SocialBtns/>
           <a className="btn-primary" href="mailto:renanmenesesdev@gmail.com" target="_blank">
             contact me
