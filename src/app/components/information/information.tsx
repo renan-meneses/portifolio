8f6ad949d15186fa8566df35dd4b08eed3045810
Renan Meneses <renanmenesesdev@gmail.com>
2023-10-20 09:31:26 -0300

    :bug: fix: sd

--- src/app/components/information/information.tsx
+++ src/app/components/information/information.tsx
@@ -21,10 +21,10 @@ export function Info(){
 
         <div className="educational-info ">
         <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
+        {/* <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
         <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
         <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
-        <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
-        <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/>
+        <Journey company = "Softmidia" office="Developer Junior" period="mai de 2019 - ago de 2019" description="Creation and development of web systems, analysis of system requirements development and data analysis."/> */}
           </div>
       </div>
     )
